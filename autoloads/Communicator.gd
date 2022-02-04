40d5971b11e444e47b4046924fc3dfe25351e3de
Angelo Aracri <angeloaracri@yahoo.de>
2022-02-05 00:52:41 +0100

    Add working send/receive lifecycle for BPM

--- autoloads/Communicator.gd
+++ autoloads/Communicator.gd
@@ -1,16 +1,19 @@
 extends Node
 
 signal connection_state_changed(is_connected)
-signal change_BPM(bpm)
+signal change_BPM(part_id, bpm)
 
-# TODO adjust url
-const _websocket_url = "ws://echo.websocket.events/.ws"
+const _websocket_url = "ws://echo.websocket.events/.ws" # TODO adjust url
 var _client = WebSocketClient.new()
 var _is_connected = false
 
-func notify_BPM(bpm):
-	# TODO better format
-	_send_data("bpm is now " + str(bpm))
+func notify_BPM(part_id, bpm):
+	var msg = {
+		"intent" : "change_bpm",
+		"part_id" : part_id,
+		"value" : bpm
+	}
+	_send_data(JSON.print(msg))
 
 func _ready():
 	_client.connect("connection_closed", self, "_closed")
@@ -34,15 +37,24 @@ func _connected(proto = ""):
 	print("Connected with protocol: ", proto)
 	_is_connected = true
 	emit_signal("connection_state_changed", _is_connected)
-	# TODO remove test packet send
-	_send_data("Test packet")
 
 func _send_data(message: String):
 	if _is_connected:
+		print("SEND:    ", message)
 		_client.get_peer(1).put_packet(message.to_utf8())
 		
 func _on_data():
-	print("Got data from server: ", _client.get_peer(1).get_packet().get_string_from_utf8())
+	var data_str = _client.get_peer(1).get_packet().get_string_from_utf8()
+	var result = JSON.parse(data_str)
+	print("RECEIVE: ", data_str)
+	
+	if result.error != OK:
+		printerr("Error parsing JSON: " + result.error_string)
+	else:
+		var data_json = result.result
+		match data_json.intent:
+			"change_bpm":
+				emit_signal("change_BPM", data_json.part_id, data_json.value)
 
 func _process(delta):
 	_client.poll()
--- scenes/part/Part.gd
+++ scenes/part/Part.gd
@@ -6,6 +6,7 @@ signal setting_sig_lower_changed(sig_lower)
 signal setting_bpm_changed(bpm)
 signal setting_bars_changed(bars)
 
+var part_id = randi() # TODO
 var setting_sig_upper = 4
 var setting_sig_lower = 4
 var setting_bpm = 120
@@ -28,38 +29,23 @@ func _ready():
 	input_sig_lower.text = str(setting_sig_lower)
 	input_bpm.text = str(setting_bpm)
 	input_bars.text = str(setting_bars)
+	
+	Communicator.connect("change_BPM", self, "_on_Communicator_change_BPM")
 
 func _on_ButtonTrackAdd_pressed():
 	pass # Replace with function body.
 
-func _on_LineEditSigUpper_text_entered(new_text):
-	var new_int = int(new_text)
-	if new_int != setting_sig_upper:
-		setting_sig_upper = new_int
-		emit_signal("setting_sig_upper_changed", setting_sig_upper)
-func _on_LineEditSigUpper_focus_exited():
-	_on_LineEditSigUpper_text_entered(input_sig_lower.text)
-
-func _on_LineEditSigLower_text_entered(new_text):
-	var new_int = int(new_text)
-	if new_int != setting_sig_lower:
-		setting_sig_lower = new_int
-		emit_signal("setting_sig_upper_changed", setting_sig_lower)
-func _on_LineEditSigLower_focus_exited():
-	_on_LineEditSigLower_text_entered(input_sig_upper.text)
 
 func _on_LineEditBPM_text_entered(new_text):
-	var new_int = int(new_text)
-	if new_int != setting_bpm:
-		setting_bpm = new_int
-		emit_signal("setting_bpm", setting_bpm)
+	Communicator.notify_BPM(part_id, int(new_text))
+	
 func _on_LineEditBPM_focus_exited():
-	_on_LineEditSigUpper_text_entered(input_bpm.text)
-
-func _on_LineEditBars_text_entered(new_text):
-	var new_int = int(new_text)
-	if new_int != setting_bars:
-		setting_bars = new_int
-		emit_signal("setting_bars", setting_bars)
-func _on_LineEditBars_focus_exited():
-	_on_LineEditSigUpper_text_entered(input_bars.text)
+	_on_LineEditBPM_text_entered(input_bpm.text)
+	
+func _on_Communicator_change_BPM(part_id, bpm):
+	if self.part_id != part_id:
+		return
+	setting_bpm = bpm
+	input_bpm.text = str(bpm)
+	emit_signal("setting_bpm_changed", bpm)
+	print("BPM was set to " + str(bpm))
