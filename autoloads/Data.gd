f486397239e39ca642c81ed60fc5b294f1b7f935
Matthias Schmitt <male.schmitt@posteo.de>
2022-02-05 23:24:36 +0100

    Fix Data (de)serialization

--- autoloads/Data.gd
+++ autoloads/Data.gd
@@ -11,10 +11,24 @@ class Lobby:
 		for u in dict['users']:
 			var user = User.from_dict(u)
 			lobby.users.append(user)
+		#print(dict['parts'])
 		for t in dict['parts']:
 			var part = Part.from_dict(t)
 			lobby.parts.append(part)
 		return lobby
+
+	func to_dict() -> Dictionary:
+		var d = {}
+		d['id'] = id
+		d['users'] = []
+		for u in users:
+			var user = u.to_dict()
+			d["users"].append(user)
+		d["parts"] = []
+		for p in parts:
+			var part = p.to_dict()
+			d["parts"].append(part)
+		return d
 
 class User:
 	var id: String
@@ -26,24 +40,11 @@ class User:
 		user.alias = "Alias" # dict['alias'] TODO
 		return user
 
-class Track:
-	var id: String
-	var muted: bool
-	var beats: Array
-	var sample_id: int = 0
-	func _init(part_data):
-		var num_beats = part_data.sig_lower * part_data.bars
-		beats = []
-		for i in range(num_beats):
-			beats.append(false)
-			
-	func from_dict(dict: Dictionary, part: Part) -> Track:
-		var track = Track.new(part)
-		track.muted = dict['muted']
-		track.sample_id = dict['sample_id']
-		# TODO init read beats from dict
-		# track.beats
-		return track
+	func to_dict() -> Dictionary:
+		var d = {}
+		d['id'] = id
+		d['alias'] = alias
+		return d
 
 class Part:
 	var id: String
@@ -58,8 +59,8 @@ class Part:
 		Track.new(self),
 		Track.new(self)
 	]
-	
-	func from_dict(dict: Dictionary) -> Part:
+
+	static func from_dict(dict: Dictionary) -> Part:
 		var part = Part.new()
 		part.id = dict["id"]
 		part.bpm = dict["bpm"]
@@ -70,6 +71,93 @@ class Part:
 		#part.time = dict["time"]
 		#part.time_last = dict["time_last"]
 		for t in dict["tracks"]:
-			var track = Track.from_dict(t)
+			var track = Track.from_dict(t, part)
 			part.tracks.append(track)
 		return part
+
+	func to_dict() -> Dictionary:
+		var d = {}
+		d["id"] = id
+		d["bpm"] = bpm
+		d["bars"] = bars
+		d["sig_upper"] = sig_upper
+		d["sig_lower"] = sig_lower
+		d["time"] = time
+		d["time_last"] = time_last
+		d["tracks"] = []
+		for t in tracks:
+			d["tracks"].append(t.to_dict())
+		return d
+
+class Track:
+	var id: String
+	var muted: bool
+	var beats: Array
+	var sample_id: int = 0
+	func _init(part_data):
+		var num_beats = part_data.sig_lower * part_data.bars
+		beats = []
+		for i in range(num_beats):
+			beats.append(false)
+			
+	static func from_dict(dict: Dictionary, part: Part) -> Track:
+		var track = Track.new(part)
+		track.id = dict['id']
+		track.muted = dict['muted']
+		track.sample_id = dict['sample_id']
+		# TODO init read beats from dict
+		# track.beats
+		return track
+
+	func to_dict() -> Dictionary:
+		var d = {}
+		d['id'] = id
+		d['muted'] = muted
+		d['sample_id'] = sample_id
+		# TODO save beats
+		# track.beats
+		return d
+
+
+var initial_state: Lobby = Lobby.from_dict(initial_lobby("i-am-a-jam-bee"))
+
+func initial_lobby(id: String) -> Dictionary:
+	return {
+	"id": id,
+	"users": [],
+	"parts": [
+		initial_part("Part 1"),
+		initial_part("Part 2"),
+		initial_part("Part 3"),
+	]
+}
+
+func initial_part(id: String):
+	return {
+		"id": id,
+		"bpm": 120,
+		"bars": 4,
+		"sig_upper": 4,
+		"sig_lower": 4,
+		"tracks": [
+			initial_track("Track 1")
+		]
+	}
+
+func initial_track(id: String):
+	return {
+		"id": id,
+		"muted": false,
+		"sample_id": 0,
+		"beats": [
+			initial_beat(), initial_beat(), initial_beat(), initial_beat(),
+			initial_beat(), initial_beat(), initial_beat(), initial_beat(),
+			initial_beat(), initial_beat(), initial_beat(), initial_beat(),
+			initial_beat(), initial_beat(), initial_beat(), initial_beat(),
+		]
+	}
+
+func initial_beat():
+	return {
+		"is_on": false
+	}
