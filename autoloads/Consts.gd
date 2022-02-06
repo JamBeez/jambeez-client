edc583b1a6218ca137f0fa5a4a6a1ddea2db751a
Angelo Aracri <angeloaracri@yahoo.de>
2022-02-06 02:15:09 +0100

    Update server url

--- autoloads/Consts.gd
+++ autoloads/Consts.gd
@@ -3,7 +3,8 @@ extends Node
 const HTTP_SERVER_URL = "http://localhost:8001/Jambeez-client.html"
 #const WS_SERVER_URL = "ws://echo.websocket.events/.ws"
 #const WS_SERVER_URL = "ws://localhost:8080/jambeez"
-const WS_SERVER_URL = "ws://vps.fuchss.org:8888/jambeez"
+#const WS_SERVER_URL = "ws://vps.fuchss.org:8888/jambeez"
+const WS_SERVER_URL = "wss://ci.fuchss.org/jambeez"
 
 
 
